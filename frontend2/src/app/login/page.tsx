474deb60764f08aa0133a68a35ed0797bb926815
Maximilian Patalas <maksymilianpatalas@gmail.com>
2024-03-19 15:06:20 +0100

    WIP

---
+++ frontend2/src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from "@/components/ui/modetoggle"
+
+
+export default function Login() {
+    
+    return(
+        <div>
+            <ModeToggle></ModeToggle>
+            <div>Login</div>
+        </div>
+        
+    ) 
+  }
--- frontend2/src/app/page.tsx
+++ frontend2/src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
 import { ModeToggle } from "@/components/ui/modetoggle";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  //redirect('/login');
   return (
     <main>
       <Button>Click me</Button>
